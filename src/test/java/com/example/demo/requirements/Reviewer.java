a91d198b963c0ba2fb8a0d2d740e2677996e65d6
linux <linux@linux>
2024-01-23 18:21:28 +0530

    g&S

--- src/test/java/com/example/demo/requirements/Reviewer.java
+++ src/test/java/com/example/demo/requirements/Reviewer.java
@@ -35,6 +35,46 @@ public class Reviewer {
 		this.dob = dob;
 		this.email = email;
 	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 	
